cff7f0588d526fdd2dfe591d2c0c3df4e4beacb4
Jecelyn Yeen <jec@google.com>
2022-10-17 01:55:17 +0200

    push

---
+++ test/specs/order-a-coffee-simple-wdio.js
@@ -0,0 +1,10 @@
+describe("order-a-coffee-simple", () => {
+  it("tests order-a-coffee-simple", async () => {
+    await browser.setWindowSize(600, 1041)
+    await browser.url("https://coffee-cart.netlify.app/")
+    await expect(browser).toHaveUrl("https://coffee-cart.netlify.app/")
+    await browser.$("[data-test='Espresso'").click()
+    // console.log(browser.sessionId);
+    // await browser.debug()
+  });
+});
